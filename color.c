5278fe6bd5d241791d3ed41f974cf52e02f80a36
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2026-06-27 18:43:49 +0800

    Update color.c

--- color.c
+++ color.c
@@ -166,7 +166,7 @@ void impact(){
  char impact0 = "element1 + element2 + element3 + element4";
  char impact1 = "element5 - element6 - element7 - element8";
  char impact2 = "element9 * element10 * element11 *element12";
- char impact3 = "element13 / element14 / element15 /  element16";
+ char impact3 = "element13 / element14 / element15 / element16";
  char impact4 = "element17 % element18 % element19 % element20";
  char impact5 = "impact0 * impact1 + impact2 * impact3";
  char impact6 = "impact1 - impact2 + impact3 - impact4";
